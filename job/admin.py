d59e9db4f2cd281013f850cc906e5f23a5696492
Gheryani101 <mohamed.elgheryani2000@gmail.com>
2023-12-12 23:24:14 +0200

    add job viwes & urls

--- job/admin.py
+++ job/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from  .models import job
+from  .models import job , category
 
 
 admin.site.register(job)
+
+
+admin.site.register(category)
